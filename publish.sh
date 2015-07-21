00385332c1a21ee93542ead597c62e2d5507663b
Sam Stoelinga <sammiestoel@gmail.com>
2015-07-21 10:32:37 +0800

    Added reserved_host_memory param to nova.conf

--- publish.sh
+++ publish.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 proxychains scp * root@10.20.0.2:~/ansible-playbook/
-proxychains ssh root@10.20.0.2 "cd ansible-playbook && ansible-playbook -i inventory playbook.yml"
+#proxychains ssh root@10.20.0.2 "cd ansible-playbook && ansible-playbook -i inventory playbook.yml"
